86c1017fad472b932e116686e4a1f6979c093adc
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-03-10 13:50:44 -0700

    more tests

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -3,6 +3,9 @@
 import unittest
 from models.amenity import Amenity
 from models.base_model import BaseModel
+from models import storage
+from datetime import datetime, timedelta
+
 
 class TestAmenity(unittest.TestCase):
     """unittests various user methods"""
@@ -39,8 +42,38 @@ class TestAmenity(unittest.TestCase):
         amenity_inst = Amenity()
         initl_upd_at = amenity_inst.updated_at
         amenity_inst.save()
-        self.assertNotEqual(initl_upd_at, amenity_inst.updated_at)
+        second_amenity = Amenity(id=amenity_inst.id)
+        storage.reload()
+        self.assertNotEqual(initl_upd_at, second_amenity.updated_at)
+        time_difference = abs(second_amenity.updated_at - initl_upd_at)
+        tolerance = timedelta(seconds=1)
+        self.assertLessEqual(time_difference, tolerance)
 
+    def test_amenity_additional_attributes(self):
+        amenity_inst = Amenity()
+        amenity_inst.custom_attribute = "Custom Value"
+        amenity_inst.save()
+        amenity_dict = amenity_inst.to_dict()
+        self.assertIn('custom_attribute', amenity_dict)
+
+    def test_amenity_save_with_arg(self):
+        amenity_inst = Amenity()
+        with self.assertRaises(TypeError):
+            amenity_inst.save(None)
+
+    def test_amenity_save_updates_file(self):
+        amenity_inst = Amenity()
+        amenity_inst.save()
+        amenity_id = "Amenity." + amenity_inst.id
+        with open("file.json", "r") as f:
+            self.assertIn(amenity_id, f.read())
+
+        second_amenity = Amenity()
+        second_amenity.save()
+        with open("file.json", "r") as f:
+            file_content = f.read()
+            self.assertIn(amenity_id, file_content)
+            self.assertIn("Amenity." + second_amenity.id, file_content)
 
 
 if __name__ == '__main__':
